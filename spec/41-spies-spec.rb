7c5be59245c1ad56782f88276319453b0e1ea06d
Jorge-Ortiz-M <jorge.ortiz@icalialabs.com>
2022-06-18 18:33:38 -0500

    Spies. This method allows us to create several methods without declare them. In other examples, we declare methods in the double method or using classes, but, with spy, we don't need to do that anymore.

---
+++ spec/41-spies-spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe 'Spie' do
+    let(:animal) { spy("animal") }
+
+    it 'confirms that a message has been received' do
+        animal.eat_food
+        expect(animal).to have_received(:eat_food)
+        expect(animal).not_to have_received(:eat_human)
+    end
+
+    it 'resets between examples' do
+        expect(animal).not_to have_received(:eat_food)
+    end
+
+    it 'reatain the same funcitonality of a regular double' do
+        animal.eat_food
+        animal.eat_food
+        animal.eat_food('Sushi')
+        expect(animal).to have_received(:eat_food).at_least(2).times
+        expect(animal).to have_received(:eat_food).exactly(3).times
+    end
+end
+
+
+=begin
+
+
+=end
